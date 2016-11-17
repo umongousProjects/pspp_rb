1601757678af7dad71b0829cad60786eb6dbbbd8
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2016-11-17 12:49:09 +0200

    locales env copy-pasted from docker

--- lib/pspp_rb/pspp/exec.rb
+++ lib/pspp_rb/pspp/exec.rb
@@ -42,9 +42,20 @@ module PsppRb
 
       def env
         { 'LANG' => 'en_US.UTF-8',
-          'LANGUAGE' => 'en_US.UTF-8',
+          'LANGUAGE' => '',
           'LC_CTYPE' => 'en_US.UTF-8',
-          'LC_ALL' => 'en_US.UTF-8' }.freeze
+          'LC_NUMERIC' => 'en_US.UTF-8',
+          'LC_TIME' => 'en_US.UTF-8',
+          'LC_COLLATE' => 'en_US.UTF-8',
+          'LC_MONETARY' => 'en_US.UTF-8',
+          'LC_MESSAGES' => 'en_US.UTF-8',
+          'LC_PAPER' => 'en_US.UTF-8',
+          'LC_NAME' => 'en_US.UTF-8',
+          'LC_ADDRESS' => 'en_US.UTF-8',
+          'LC_TELEPHONE' => 'en_US.UTF-8',
+          'LC_MEASUREMENT' => 'en_US.UTF-8',
+          'LC_IDENTIFICATION' => 'en_US.UTF-8',
+          'LC_ALL' => '' }.freeze
       end
     end
   end
