361a2e0d98f31bffab5044b9c592036b8893e892
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2016-11-17 16:13:06 +0200

    en_CA locale

--- lib/pspp_rb/pspp/exec.rb
+++ lib/pspp_rb/pspp/exec.rb
@@ -41,21 +41,8 @@ module PsppRb
       end
 
       def env
-        { 'LANG' => 'en_US.UTF-8',
-          'LANGUAGE' => 'en_US.UTF-8',
-          'LC_CTYPE' => 'en_US.UTF-8',
-          'LC_NUMERIC' => 'en_US.UTF-8',
-          'LC_TIME' => 'en_US.UTF-8',
-          'LC_COLLATE' => 'en_US.UTF-8',
-          'LC_MONETARY' => 'en_US.UTF-8',
-          'LC_MESSAGES' => 'en_US.UTF-8',
-          'LC_PAPER' => 'en_US.UTF-8',
-          'LC_NAME' => 'en_US.UTF-8',
-          'LC_ADDRESS' => 'en_US.UTF-8',
-          'LC_TELEPHONE' => 'en_US.UTF-8',
-          'LC_MEASUREMENT' => 'en_US.UTF-8',
-          'LC_IDENTIFICATION' => 'en_US.UTF-8',
-          'LC_ALL' => 'en_US.UTF-8' }.freeze
+        { 'LANG' => 'C',
+          'LC_ALL' => 'en_CA.UTF-8' }.freeze
           {}
       end
     end
